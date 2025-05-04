d4fef576dbba5dd810634fdf50cc5f1edd760be5
Shafayet Ahmmed <shafayet.dev@gmail.com>
2025-05-04 23:26:41 +0600

    issue fix

---
+++ studio/lib/env.ts
@@ -0,0 +1,24 @@
+const isDevelopment = process.env.NODE_ENV === "development";
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-11-10";
+
+export const dataset = assertValue(
+  isDevelopment
+    ? process.env.NEXT_PUBLIC_SANITY_DATASET_DEVELOPMENT
+    : process.env.NEXT_PUBLIC_SANITY_DATASET_PRODUCTION,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+
+  return v;
+} 
